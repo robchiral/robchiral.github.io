e93d69e0001d739866ac6529577f92cb0ba72f9d
Robert Chen <51394230+robchiral@users.noreply.github.com>
2025-11-24 07:55:00 -0500

    Fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robche.com',
+  base: '/robchiral.github.io',
   integrations: [sitemap()],
   build: {
     assets: '_assets'
