7f7570adc3e211edffb2d4a0a16081a39556c866
Robert Chen <51394230+robchiral@users.noreply.github.com>
2025-11-24 07:59:51 -0500

    Fix css loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robche.com',
-  base: '/robchiral.github.io',
   integrations: [sitemap()],
   build: {
     assets: '_assets'
